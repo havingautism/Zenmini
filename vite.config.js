2756a469257fae6480edb7495568db7fd2f2800c
havingautism <havingautism@outlook.com>
2025-11-20 22:11:53 +0800

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/gemini_chat/',  // GitHub Pages 子路径
+  base: '/Zenmini/',  // 匹配 GitHub Pages 实际URL
   plugins: [react()],
   server: {
     host: true
